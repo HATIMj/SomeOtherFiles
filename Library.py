bfae498b0bb5a8fa823303c33fe939682e6dd5aa
Jashandeep singh <jashandeeps@localhost.localdomain>
2021-09-19 19:59:43 +0530

    mini project

--- Library.py
+++ Library.py
@@ -1,50 +1,45 @@
 class Library:
     def __init__(self,list,name):
-       self.list=list
-       self.name=name
-    def show(self):
+        self.list=list
+        self.name=name
+        self.dic={}
+    def displaybooks(self):
         for i in self.list:
             print(i)
-    def add(self,abook):
-        self.book=abook
-
-        for i in range(1):
-            self.list.append(self.book)
+    def lendbook(self,user,book):
+        if book not in self.dic.keys():
+            self.dic.update({book:user})
+        else:
+            print("Book is already present in list.")
+    def addbook(self,book):
+        self.list.append(book)
+    def returnbook(self,book):
         
-    def lend(self,lbook):
-        self.book=lbook
-        for i in self.list:
-            if i==self.book:
-                v.append(i)
-                self.list.remove(i)
-                c.append(self.name)
-    def returnb(self,lbook):
-        self.book=lbook
-        for i in v:
-            if i==self.book:
-                v.remove(i)
-                self.list.append(i)
-            else:
-                print("no one have lended this book.")
-
-    def show_dict(self):
-        u=v
-        q=c
-        pass
-
-
-
-
-
-
-
-
-
-
-
-v=[]
-c=[]
-d={}
+        self.dic.pop(book)
 if __name__=='__main__':
-    s=list(map(str,input("Books name separated by space:--").split(" ")))
-    
+    j=Library(["rich","honey","jungle book"],"jashan")
+    while True:
+       print("choose a function to peform:")
+       i=int(input(""))
+       if i==1:
+           j.displaybooks()
+       elif i==2:
+           user=input("our id :")
+           book=input("which book you want to lend:")
+           j.lendbook(user,book)
+       elif i==3:
+           book=input("name of the book you want to add:")
+           j.addbook(book)
+       elif i==4:
+           book=input("book you want to return:")
+       else:
+           print("Error")
+       print("q to quit or c to continue")
+       i2=""
+       while i2!="q" and i2!="c":
+           i2=input()
+           if i2=="q":
+               exit()
+           elif i2=="c":
+               continue
+        
